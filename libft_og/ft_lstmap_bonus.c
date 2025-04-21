364ab8d01ca376fa80e3e96fdfc890ce23153332
Alberto <adiazufanomarti@gmail.com>
2025-04-21 10:50:10 +0200

    finish

--- libft_og/ft_lstmap_bonus.c
+++ libft_og/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aldiaz-u <aldiaz-u@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 20:26:39 by aldiaz-u          #+#    #+#             */
-/*   Updated: 2025/04/21 10:10:29 by aldiaz-u         ###   ########.fr       */
+/*   Updated: 2025/04/21 10:47:37 by aldiaz-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_list;
 	t_list	*new_node;
 
+	if(!lst || !f)
+		return (NULL);
 	new_list = NULL;
 	while (lst)
 	{
-		new_node = malloc(sizeof(t_list));
+		new_node = ft_lstnew(f(lst->content));
 		if (!new_node)
 		{
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		new_node -> content = f(lst->content);
-		new_node->next = NULL;
 		ft_lstadd_back(&new_list, new_node);
 		lst = lst -> next;
 	}
